857a28d27583cc02d19da4005fa051a2ead432dc
Storm-Julian Tersteeg <stormtersteeg@gmail.com>
2025-12-06 19:54:12 +0000

    replace quality select with range for more intuitive input

--- app/components/cards/optionsCard.tsx
+++ app/components/cards/optionsCard.tsx
@@ -1,11 +1,12 @@
 import type { HTMLAttributes } from "react";
+import { useGlobalStore } from "~/stores/globalStore";
 import { useOptionStore } from "~/stores/optionsStore";
-import type CompressionQuality from "~/types/compressionQuality";
-import { CompressionQualityRange } from "~/types/compressionQuality";
 
 export default function OptionsCard({
   className,
 }: HTMLAttributes<HTMLDivElement>) {
+  const setOverflow = useGlobalStore((state) => state.setOverflow);
+  const quality = useOptionStore((s) => s.quality);
   const setQuality = useOptionStore((s) => s.setQuality);
   const setMaxWidth = useOptionStore((s) => s.setMaxWidth);
   const setMaxHeight = useOptionStore((s) => s.setMaxHeight);
@@ -20,18 +21,30 @@ export default function OptionsCard({
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-x-8 gap-y-4">
         <fieldset className="fieldset">
-          <legend className="fieldset-legend">Quality</legend>
-          <select
-            defaultValue={CompressionQualityRange[2].value}
-            className="select w-full md:w-auto"
-            onChange={(e) => setQuality(parseFloat(e.target.value))}
-          >
-            {CompressionQualityRange.map((n: CompressionQuality) => (
-              <option key={n.name} value={n.value}>
-                {n.name} {n.value == 0.7 ? "(Recommended)" : ""}
-              </option>
-            ))}
-          </select>
+          <legend className="fieldset-legend">
+            Quality {(quality * 100).toFixed(0)}%
+          </legend>
+          <div className="w-full">
+            <input
+              type="range"
+              min={0}
+              max={99}
+              step={1}
+              className="range w-full"
+              value={quality * 100}
+              onPointerDown={() => setOverflow(false)}
+              onPointerUp={() => setOverflow(true)}
+              onPointerCancel={() => setOverflow(true)}
+              onChange={(e) => {
+                setQuality(parseInt(e.target.value) / 100);
+              }}
+            />
+            <div className="flex justify-between px-2.5 mt-2 text-xs">
+              {Array.from({ length: 6 }, (_, i) => (
+                <span key={i}>{i != 5 ? i * 20 : 99}</span>
+              ))}
+            </div>
+          </div>
         </fieldset>
 
         <fieldset className="fieldset">
