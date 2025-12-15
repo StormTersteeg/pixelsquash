adb762df7d058e171dd6ba4cb8746354b4d7ff71
Storm-Julian Tersteeg <stormtersteeg@gmail.com>
2025-12-15 22:10:38 +0000

    added simple onload animations

--- app/components/standard/footer.tsx
+++ app/components/standard/footer.tsx
@@ -1,6 +1,14 @@
 export default function Footer() {
   return (
-    <footer className="footer sm:footer-horizontal footer-center bg-gray-950 text-base-content text-gray-600 p-4">
+    <footer
+      className="
+        footer sm:footer-horizontal footer-center
+        bg-gray-950 text-base-content text-gray-600 p-4
+        animate-in fade-in slide-in-from-bottom-4 duration-700
+        fill-mode-both
+        [animation-delay:1000ms]
+      "
+    >
       <aside>
         <p>Created by Storm-Julian Tersteeg - 2025</p>
       </aside>
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,9 +5,9 @@ import OptionsCard from "~/components/cards/optionsCard";
 export default function Home() {
   return (
     <div className="container mx-auto px-0 md:px-8">
-      <ImageSelectionCard className="mt-8" />
-      <OptionsCard className="mt-8" />
-      <ExportCard className="mt-8 mb-8" />
+      <ImageSelectionCard className="animate-in fade-in slide-in-from-bottom-4 duration-700 mt-8" />
+      <OptionsCard className="animate-in fade-in slide-in-from-bottom-4 duration-700 mt-8" />
+      <ExportCard className="animate-in fade-in slide-in-from-bottom-4 duration-700 mt-8 mb-8" />
     </div>
   );
 }
